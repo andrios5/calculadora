7af9ecbf7311746e5a1b2a3ed50cc03f5b34f283
andrios5 <andrios.geleinski@gmail.com>
2025-06-18 11:18:53 -0300

    New Update

--- dinheiroGasto.js
+++ dinheiroGasto.js
@@ -38,15 +38,19 @@ document.querySelector('#maisV').addEventListener('click', mais)
 document.querySelector('#menosV').addEventListener('click', menos)
 document.querySelector('#resetV1').addEventListener('click', resetvalor1)
 document.querySelector('#resetV2').addEventListener('click', resetvalor2)
+document.querySelector('#setaGasto').addEventListener('click', menuTeto)
+document.querySelector('button#definirTeto').addEventListener('click', defineTeto)
 
 telaV1 = document.querySelector('section.telaV1')
 telaV0 = document.querySelector('section.telaV0')
 telaV2 = document.querySelector('section.telaV2')
 telaV3 = document.querySelector('section.telaV3')
 telaV4 = document.querySelector('section.telaV4')
+telaV5 = document.querySelector('section.telaV5')
 
 let valorDia = 0 // Inicializa o valor do dia
 let valorMes = 0 // Inicializa o valor do mês
+let tempV1 = false
 valorDia = Number(getCookie('valorDia')) // Lê o cookie do valor do dia
 valorDia1 = valorDia.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
 telaV0.innerHTML = `${valorDia1}`
@@ -84,6 +88,72 @@ if (valorMesAnt != '' && valorMesAnt > 0) { // Se não houver valor do mês ante
     telaV4.style.display = 'none' // Esconde a tela do valor do mês anterior
 }
 
+verificaTeto()
+
+function menuTeto() {
+    
+    if (tempV1 == false) { // Verifica se o menu está fechado
+        document.querySelector('#setaGasto').style.transform = 'rotate(180deg)' // Rotaciona o ícone para baixo
+        document.querySelector('input#tetoGasto').style.display = 'inline-block' // Exibe o input do teto de gastos
+        document.querySelector('button#definirTeto').style.display = 'inline-block' // Exibe o botão para definir o teto de gastos
+        tempV1 = true // Atualiza o estado do menu para aberto
+    } else {
+        document.querySelector('#setaGasto').style.transform = 'rotate(0deg)' // Rotaciona o ícone para cima
+        document.querySelector('input#tetoGasto').style.display = 'none' // Esconde o input do teto de gastos
+        document.querySelector('button#definirTeto').style.display = 'none' // Esconde o botão para definir o teto de gastos
+        tempV1 = false // Atualiza o estado do menu para fechado
+    }
+}
+
+function defineTeto() {
+    tetoGasto = Number(document.querySelector('#tetoGasto').value) // Lê o valor do input
+    setCookie('tetoGasto', tetoGasto, 30) // Salva o teto de gastos no cookie
+    alert(`Teto de gastos definido: ${tetoGasto.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`) // Exibe o teto de gastos definido
+    document.querySelector('#tetoGasto').value = '' // Limpa o input do teto de gastos
+    verificaTeto()
+}
+
+function verificaTeto() {
+    tetoGasto = Number(document.querySelector('#tetoGasto').value) // Lê o valor do input
+    tempTeto = Number(getCookie('tetoGasto')) // Lê o cookie do teto de gastos
+    if (tetoGasto == '' || tetoGasto <= 0) { // Verifica se o valor é válido
+        tetoGasto = tempTeto}
+        if (tetoGasto == '' || tetoGasto == 0) { // Se o teto de gastos não foi definido, não exibe a tela
+            telaV5.style.display = 'none' // Esconde a tela da porcentagem do teto de gastos
+        }
+        else if (tetoGasto > 0) {
+        porTeto = (valorMes / tetoGasto) * 100 // Calcula a porcentagem do teto de gastos
+        porTeto = porTeto.toFixed(2) // Formata a porcentagem para duas casas decimais
+        if (porTeto <= 50) { // Verifica se o valor do mês ultrapassa o teto de gastos
+            telaV5.innerHTML = `Você gastou ${porTeto}% do seu limite de gastos!` // Exibe a porcentagem do teto de gastos
+            telaV5.innerHTML += `<p id="teto">Seu limite é de ${tetoGasto.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>` // Exibe o teto de gastos
+            telaV5.style.display = 'block' // Exibe a tela da porcentagem do teto de gastos
+            telaV5.style.backgroundColor = '#00FF00' // Define a cor de fundo como verde
+            telaV5.style.color = '#363636' // Define a cor da fonte como preto
+        } else if (porTeto > 50 && porTeto <= 75) {
+            telaV5.innerHTML = `Você gastou ${porTeto}% do seu limite de gastos!` // Exibe a porcentagem do teto de gastos
+            telaV5.innerHTML += `<p id="teto">Seu limite é de ${tetoGasto.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>` // Exibe o teto de gastos
+            telaV5.style.display = 'block' // Exibe a tela da porcentagem do teto de gastos
+            telaV5.style.backgroundColor = 'yellow' // Define a cor da fonte como amarelo
+            telaV5.style.color = '#363636' // Define a cor da fonte como preto
+        } else if (porTeto > 75 && porTeto < 100) {
+            telaV5.innerHTML = `Você gastou ${porTeto}% do seu limite de gastos!` // Exibe a porcentagem do teto de gastos
+            telaV5.innerHTML += `<p id="teto">Seu limite é de ${tetoGasto.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>` // Exibe o teto de gastos
+            telaV5.style.display = 'block' // Exibe a tela da porcentagem do teto de gastos
+            telaV5.style.backgroundColor = '#FFA500' // Define a cor da fonte como amarelo
+        } else if (porTeto >= 100) {
+            telaV5.innerHTML = `Você ultrapassou o teto de gastos em ${porTeto}%` // Exibe a mensagem de alerta
+            telaV5.innerHTML += `<p id="teto">Seu limite é de ${tetoGasto.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>` // Exibe o teto de gastos
+            telaV5.style.display = 'block' // Exibe a tela da porcentagem do teto de gastos
+            telaV5.style.backgroundColor = '#FF0000' // Define a cor da fonte como vermelho
+            telaV5.style.color = '#FFFFFF' // Define a cor da fonte como branco
+        } else {
+            telaV5.style.display = 'none' // Esconde a tela da porcentagem do teto de gastos
+        }
+
+    }
+}
+
 
 function mais() {
     diaInicial = getCookie('diaInicial')
@@ -109,6 +179,7 @@ function mais() {
             dataInicialV() // Se não houver data inicial salva, chama a função para salvar
         }
     }
+    verificaTeto()
 }
 }
 
@@ -132,6 +203,7 @@ function menos() {
             dataInicialV() // Se não houver data inicial salva, chama a função para salvar
         }
     }
+    verificaTeto()
 }
 
 
@@ -170,6 +242,8 @@ function resetvalor2() {
     setCookie('diaAnteriorV', diaAnteriorV, 60) // Salva o dia anterior no cookie
     setCookie('valorMes', valorMes, 90) // Reseta o valor do mês no cookie
     telaV2.innerHTML = `Valor do mês: 0`
+    tetoGasto = 0 // Reseta o teto de gastos
+    setCookie('tetoGasto', tetoGasto, 30) // Reseta o teto de gastos no cookie
     dataInicialV()
     resetvalor1() // Reseta o valor do dia
 }
